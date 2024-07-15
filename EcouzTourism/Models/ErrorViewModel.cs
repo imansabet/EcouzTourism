0b810b59ed8e3b7da478a1a444e9eef14cb84968
iman <dev.imansabet@gmail.com>
2024-07-15 11:42:55 +0330

    Add project files.

---
+++ EcouzTourism/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcouzTourism.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
