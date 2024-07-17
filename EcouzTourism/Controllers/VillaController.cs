e6fa89d4e0fc612ea40c14d2bc3f4cc98c1aefd6
iman <dev.imansabet@gmail.com>
2024-07-17 21:45:19 +0330

    Inject Villa Repository in controller instead dbContext

--- EcouzTourism/Controllers/VillaController.cs
+++ EcouzTourism/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
-﻿using EcouzTourism.Domain.Entities;
+﻿using EcouzTourism.Application.Common.Interfaces;
+using EcouzTourism.Domain.Entities;
 using EcouzTourism.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,15 +7,15 @@ namespace EcouzTourism.Controllers
 {
     public class VillaController : Controller
     {
-        private readonly ApplicationDbContext _db;
+        private readonly IVillaRepository _villaRepo;
 
-        public VillaController(ApplicationDbContext db)
+        public VillaController(IVillaRepository villaRepo)
         {
-            _db = db;
+            _villaRepo = villaRepo;
         }
         public IActionResult Index()
         {
-            var villas = _db.Villas.ToList();
+            var villas = _villaRepo.GetAll();
             return View(villas);
         }
         public IActionResult Create()
@@ -32,8 +33,8 @@ namespace EcouzTourism.Controllers
             if (ModelState.IsValid)
             {
 
-                _db.Villas.Add(obj);
-                _db.SaveChanges();
+                _villaRepo.Add(obj);
+                _villaRepo.Save();
                 TempData["success"] = $"{ obj.Name } Has been created successfully";
                 return RedirectToAction("Index");
             }
@@ -42,7 +43,7 @@ namespace EcouzTourism.Controllers
 
         public IActionResult Update(int villaId)
         {
-            Villa? obj = _db.Villas.FirstOrDefault(u=>u.Id == villaId);
+            Villa? obj = _villaRepo.Get(u=>u.Id == villaId);
             if (obj is null)
             {
                 return RedirectToAction("Error", "Home");
@@ -55,8 +56,8 @@ namespace EcouzTourism.Controllers
             if (ModelState.IsValid && obj.Id > 0)
             {
 
-                _db.Villas.Update(obj);
-                _db.SaveChanges();
+                _villaRepo.Update(obj);
+                _villaRepo.Save();
                 TempData["success"] = $"{obj.Name } Has been updated successfully";
                 return RedirectToAction("Index");
             }
@@ -66,7 +67,7 @@ namespace EcouzTourism.Controllers
 
         public IActionResult Delete (int villaId)
         {
-            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            Villa? obj = _villaRepo.Get(u => u.Id == villaId);
             if (obj is null)
             {
                 return RedirectToAction("Error", "Home");
@@ -77,13 +78,13 @@ namespace EcouzTourism.Controllers
         [HttpPost]
         public IActionResult Delete(Villa obj)
         {
-            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+            Villa? objFromDb = _villaRepo.Get(u => u.Id == obj.Id);
     
             if (objFromDb is not null)
             {
 
-                _db.Villas.Remove(objFromDb);
-                _db.SaveChanges();
+                _villaRepo.Remove(objFromDb);
+                _villaRepo.Save();
                 TempData["success"] = $"{ objFromDb.Name } Has been deleted successfully";
                 return RedirectToAction("Index");
             }
