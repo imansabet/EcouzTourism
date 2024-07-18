64e8d40913249258eac7377fa9c6da36bda57843
iman <dev.imansabet@gmail.com>
2024-07-18 22:41:57 +0330

    implement UnitOfWork

---
+++ EcouzTourism.infrastructure/Respository/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using EcouzTourism.Application.Common.Interfaces;
+using EcouzTourism.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcouzTourism.Infrastructure.Respository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IVillaRepository Villa { get; private set; }
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Villa = new VillaRepository(_db);
+        }
+    }
+}
