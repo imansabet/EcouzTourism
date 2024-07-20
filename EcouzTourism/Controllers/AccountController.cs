aa390165f6d164f0152cc54c7b2361b3be5a2372
iman <dev.imansabet@gmail.com>
2024-07-20 21:32:10 +0330

    AccountController : Register method, Add Roles in DB, Register UI

--- EcouzTourism/Controllers/AccountController.cs
+++ EcouzTourism/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using EcouzTourism.Application.Common.Interfaces;
+using EcouzTourism.Application.Utility;
 using EcouzTourism.Domain.Entities;
 using EcouzTourism.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EcouzTourism.Controllers
 {
@@ -36,9 +38,26 @@ namespace EcouzTourism.Controllers
 
             return View(loginVM);
         }
-        public IActionResult Register()
+        public IActionResult Register(string returnUrl = null)
         {
-            return View();
+            returnUrl ??= Url.Content("~/");
+            if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).Wait();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).Wait();
+            }
+
+            RegisterVM registerVM = new()
+            {
+                RoleList = _roleManager.Roles.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Name
+                }),
+                RedirectUrl = returnUrl
+            };
+
+            return View(registerVM);
         }
     }
 }
