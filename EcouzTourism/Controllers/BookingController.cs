c460b0be4ab2b20d7822c5c621981f93629b5ad4
iman <dev.imansabet@gmail.com>
2024-07-23 19:21:43 +0330

    Get Action Finalize Booking

---
+++ EcouzTourism/Controllers/BookingController.cs
@@ -0,0 +1,31 @@
+﻿using EcouzTourism.Application.Common.Interfaces;
+using EcouzTourism.Domain.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcouzTourism.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BookingController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
+        {
+            Booking booking = new()
+            {
+                VillaId = villaId,
+                Villa = _unitOfWork.Villa.Get(u=>u.Id==villaId,includeProperties:"VillaAmenity"),
+                CheckInDate = checkInDate,
+                Nights = nights,
+                CheckOutDate = checkInDate.AddDays(nights),
+                
+            };
+            return View(booking);
+        }
+    }
+    }
+}
