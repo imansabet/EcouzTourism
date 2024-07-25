e0177b9edd390e5e747f96fb6ad00dd3c57d5ed7
iman <dev.imansabet@gmail.com>
2024-07-25 23:49:25 +0330

    Role Base UI

---
+++ EcouzTourism/Controllers/DashbordController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcouzTourism.Controllers
+{
+    public class DashbordController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
