b307b0a6fbe1bf395fe216d06b282561282bb25c
iman <dev.imansabet@gmail.com>
2024-07-27 19:10:19 +0330

    Add As No Tracking  for Get & GetAll Repository method

--- EcouzTourism.infrastructure/Respository/Repository.cs
+++ EcouzTourism.infrastructure/Respository/Repository.cs
@@ -31,9 +31,17 @@ namespace EcouzTourism.Infrastructure.Respository
             return dbSet.Any(filter);
         }
 
-        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
         {
-            IQueryable<T> query = dbSet;
+            IQueryable<T> query;
+            if (tracked)
+            {
+                query = dbSet;
+            }
+            else
+            {
+                query = dbSet.AsNoTracking();
+            }
             if (filter != null)
             {
                 query = query.Where(filter);
@@ -50,9 +58,17 @@ namespace EcouzTourism.Infrastructure.Respository
             return query.FirstOrDefault();
         }
 
-        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null, bool tracked = false)
         {
-            IQueryable<T> query = dbSet;
+            IQueryable<T> query;
+            if (tracked)
+            {
+                query = dbSet;
+            }
+            else
+            {
+                query = dbSet.AsNoTracking();
+            }
             if (filter != null)
             {
                 query = query.Where(filter);
